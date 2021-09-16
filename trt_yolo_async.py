f25cc25753574c582d933964ce4fd0771ffdcb89
FezChoi <70895582+FezChoi@users.noreply.github.com>
2021-09-16 11:11:56 +0900

    20210916 FezChoi commit

--- trt_yolo_async.py
+++ trt_yolo_async.py
@@ -8,8 +8,10 @@ TensorRT optimized YOLO engine.
 import os
 import time
 import argparse
+import pdb
 
 import threading ## Added for async
+from trt_yolo_async_class import ThreadQueue
 
 import cv2
 import pycuda.autoinit  # This is needed for initializing CUDA driver
@@ -21,36 +23,15 @@ from utils.camera import add_camera_args, Camera
 from utils.display import open_window, set_display, show_fps
 from utils.visualization import BBoxVisualization
 from utils.yolo_with_plugins import TrtYOLO
+from utils.distancing import show_distancing
+from utils.distancing_class import FrameData
 
 
 WINDOW_NAME = 'TrtYOLODemo'
 MAIN_THREAD_TIMEOUT = 20.0  # 20 seconds
 
-## pipelining 을 위한 global 변수.s
-## 각 함수들(main thread의 inference함수, child thread)에서 계속 처리해서 이미지를 저장함
-s_img, s_boxes, s_confs, s_clss = None, None, None, None
-
-def parse_args():
-    """Parse input arguments."""
-    desc = ('Capture and display live camera video, while doing '
-            'real-time object detection with TensorRT optimized '
-            'YOLO model on Jetson')
-    parser = argparse.ArgumentParser(description=desc)
-    parser = add_camera_args(parser)
-    parser.add_argument(
-        '-c', '--category_num', type=int, default=80,
-        help='number of object categories [80]')
-    parser.add_argument(
-        '-m', '--model', type=str, required=True,
-        help=('[yolov3-tiny|yolov3|yolov3-spp|yolov4-tiny|yolov4|'
-              'yolov4-csp|yolov4x-mish]-[{dimension}], where '
-              '{dimension} could be either a single number (e.g. '
-              '288, 416, 608) or 2 numbers, WxH (e.g. 416x256)'))
-    parser.add_argument(
-        '-l', '--letter_box', action='store_true',
-        help='inference with letterboxed image [False]')
-    args = parser.parse_args()
-    return args
+## pipelining 을 위한 global 변수.
+threadQueue = ThreadQueue()
 
 class TrtThread(threading.Thread):
     """TrtThread
@@ -90,21 +71,27 @@ class TrtThread(threading.Thread):
         which calls CUDA kernels.  In other words, creating CUDA
         context in __init__() doesn't work.
         """
-        global s_img, s_boxes, s_confs, s_clss
 
         print('TrtThread: loading the TRT YOLO engine...')
         self.cuda_ctx = cuda.Device(0).make_context()  # GPU 0
-        self.trt_yolo = TrtYOLO(self.model, self.category_num, self.letter_box)        #Yolo문법: trt_yolo = TrtYOLO(args.model, args.category_num, args.letter_box)
+        self.trt_yolo = TrtYOLO(self.model, self.category_num, self.letter_box)         #Yolo문법: trt_yolo = TrtYOLO(args.model, args.category_num, args.letter_box)
         print('TrtThread: start running...')
         self.running = True
         while self.running:
+            tic1 = time.time()
             img = self.cam.read()
             if img is None:
+                threadQueue.setImpossible()
                 break
-            boxes, confs, clss = self.trt_yolo.detect(img, self.conf_th)
-            with self.condition:
-                s_img, s_boxes, s_confs, s_clss = img, boxes, confs, clss       #pipeline 부분 condition(child Thread가 준비됨)이 true가 되면 사용가능
-                self.condition.notify()
+
+            trt_outputs, letter_box = self.trt_yolo.detect(img, self.conf_th)
+
+            toc1 = time.time()
+            print("child_thread : ", '{:.2f}'.format(round((toc1-tic1) * 1000, 2)).rjust(10), "ms")
+
+            threadQueue.waitMainThread()
+            threadQueue.putThreadQueue(img, trt_outputs, letter_box)
+            
         del self.trt_yolo
         self.cuda_ctx.pop()
         del self.cuda_ctx
@@ -121,36 +108,48 @@ def loop_and_detect(condition ,cam, trt_yolo, conf_th, vis):
     # Arguments
       cam: the camera instance (video source).
       trt_yolo: the TRT YOLO object detector instance.
-      conf_th: confidence/score threshold for object detection. 
+      conf_th: confidence/score threshold for object detection.
       vis: for visualization.
     """
-
-    global s_img, s_boxes, s_confs, s_clss
-
+    
     full_scrn = False
-    fps = 0.0
-    tic = time.time()
+    # fps = 0.0
+    # fps2 = 0.0
+    # tic1 = time.time()
+    frameData = FrameData()
+    frameData.set_timer()
+
     while True:
         if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
             break
-        with condition:
-            if condition.wait(timeout = MAIN_THREAD_TIMEOUT):
-                img, boxes, confs, clss = s_img, s_boxes, s_confs, s_clss
-            else:
-                raise SystemExit('ERROR: timeout waiting for img from child')
-        # Delete for async (09.14)
-        #img = cam.read()
-        #if img is None:
-        #    break
-        #boxes, confs, clss = trt_yolo.detect(img, conf_th)
-        img = vis.draw_bboxes(img, boxes, confs, clss)
-        img = show_fps(img, fps)
+
+        img, trt_outputs, letter_box = threadQueue.getThreadQueue()
+
+        if threadQueue.isImpossible() and threadQueue.isEmpty():
+            break
+
+        threadQueue.signalMainThread()
+
+
+        # toc1 = time.time()
+        # print("child_thread : ", '{:.2f}'.format(round((toc1-tic1) * 1000, 2)).rjust(10), "ms")
+
+        tic2 = time.time()
+        boxes, confs, clss = trt_yolo.detect_post(img, conf_th, trt_outputs, letter_box)
+        img = show_distancing(img, boxes, frameData)
+        img = show_fps(img, frameData.get_fps())
         cv2.imshow(WINDOW_NAME, img)
-        toc = time.time()
-        curr_fps = 1.0 / (toc - tic)
-        # calculate an exponentially decaying average of fps number
-        fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
-        tic = toc
+
+        frameData.increase_counter()
+        frameData.update_fps()
+        frameData.clear_log()
+
+        toc2 = time.time()
+        print(" main_thread : ", '{:.2f}'.format(round((toc2-tic2) * 1000, 2)).rjust(10), "ms")
+
+        # tic1 = time.time()
+        
+
         key = cv2.waitKey(1)
         if key == 27:  # ESC key: quit program
             break
@@ -158,6 +157,28 @@ def loop_and_detect(condition ,cam, trt_yolo, conf_th, vis):
             full_scrn = not full_scrn
             set_display(WINDOW_NAME, full_scrn)
 
+def parse_args():
+    """Parse input arguments."""
+    desc = ('Capture and display live camera video, while doing '
+            'real-time object detection with TensorRT optimized '
+            'YOLO model on Jetson')
+    parser = argparse.ArgumentParser(description=desc)
+    parser = add_camera_args(parser)
+    parser.add_argument(
+        '-c', '--category_num', type=int, default=80,
+        help='number of object categories [80]')
+    parser.add_argument(
+        '-m', '--model', type=str, default=str('yolov4-tiny-3l-crowdhuman-416'),
+        help=('[yolov3-tiny|yolov3|yolov3-spp|yolov4-tiny|yolov4|'
+              'yolov4-csp|yolov4x-mish]-[{dimension}], where '
+              '{dimension} could be either a single number (e.g. '
+              '288, 416, 608) or 2 numbers, WxH (e.g. 416x256)'))
+    parser.add_argument(
+        '-l', '--letter_box', action='store_true',
+        help='inference with letterboxed image [False]')
+    args = parser.parse_args()
+    return args
+    
 
 def main():
     args = parse_args()
@@ -179,13 +200,13 @@ def main():
     open_window(
         WINDOW_NAME, 'Camera TensorRT YOLO Demo',
         cam.img_width, cam.img_height)
-    vis = BBoxVisualization(cls_dict) ## modified line(161->167)
+    vis = BBoxVisualization(cls_dict) ## modified line(176->182)
     condition = threading.Condition() ## Added for async (09.14)
 
     trt_thread = TrtThread(condition, cam, args.model, args.category_num, args.letter_box, conf_th=0.3,)  ## Added for async (09.14) 
     trt_thread.start() #start the child thread
     loop_and_detect(condition, cam, trt_yolo, conf_th=0.3, vis=vis)
-    trt.thread.stop() #stop the child thread
+    trt_thread.stop() #stop the child thread
 
 
     cam.release()
