9cc2a8648f387084020d8786e7986e76e95f8dbf
FezChoi <70895582+FezChoi@users.noreply.github.com>
2021-09-29 00:49:57 +0900

    20210928 FezChoi commit

--- app_async.py
+++ app_async.py
@@ -1,7 +1,5 @@
 import cv2
 
-
-
 import pycuda.autoinit
 import pycuda.driver as cuda ## Added for async
 import threading ## Added for async
@@ -10,13 +8,14 @@ from utils.yolo_with_plugins import TrtYOLO
 from utils.distancing_class import FrameData
 from utils.distancing import show_distancing
 from app_async_class import parse_args, ThreadQueue
+from utils.camera import Camera
 
 from flask import Flask, render_template, Response
 
 
 args = parse_args()
 args.model = 'yolov4-tiny-3l-crowd-416'
-args.video = 'source_video/people-640p.mp4'
+args.video = 'source_video/people-640p.mp4' # 0 if webcam
 args.category_num = 80
 
 MAIN_THREAD_TIMEOUT = 20.0  # 20 seconds
@@ -41,7 +40,7 @@ class TrtThread(threading.Thread):
     def run(self):
         print('TrtThread: loading the TRT YOLO engine...')
         self.cuda_ctx = cuda.Device(0).make_context()  # GPU 0
-        self.trt_yolo = TrtYOLO(self.model, self.category_num, self.letter_box)         #Yolo문법: trt_yolo = TrtYOLO(args.model, args.category_num, args.letter_box)
+        self.trt_yolo = TrtYOLO(self.model, self.category_num, self.letter_box, self.cuda_ctx)         #Yolo문법: trt_yolo = TrtYOLO(args.model, args.category_num, args.letter_box)
         print('TrtThread: start running...')
         self.running = True
         
@@ -52,7 +51,7 @@ class TrtThread(threading.Thread):
                 threadQueue.setImpossible()
                 break
 
-            boxes, confs, clss = self.trt_yolo.detect(img, conf_th=0.3)
+            boxes, confs, clss = self.trt_yolo.detect(img, self.conf_th)
 
             threadQueue.putThreadQueue(img, boxes)
             
@@ -66,11 +65,7 @@ class TrtThread(threading.Thread):
         self.join()
 
 
-def gen_frames(camera):  # generate frame by frame from camera
-    condition = threading.Condition() ## Added for async (09.14)
-    trt_thread = TrtThread(condition, camera, args.model, args.category_num, args.letter_box, conf_th=0.3,)  ## Added for async (09.14) 
-    trt_thread.start() #start the child thread
-
+def gen_frames():  # generate frame by frame from camera
     frameData = FrameData()
     frameData.set_timer()
 
@@ -93,17 +88,11 @@ def gen_frames(camera):  # generate frame by frame from camera
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
-    trt_thread.stop()
 
 
 @app.route('/video_feed')
 def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
-    ## thread start
-    #trt_yolo = TrtYOLO(args.model, args.category_num, args.letter_box, cuda_ctx=pycuda.autoinit.context)
-    camera = cv2.VideoCapture(0)  # use 0 for web camera
-
-    return Response(gen_frames(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/')
@@ -113,4 +102,10 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    condition = threading.Condition() ## Added for async (09.14)
+    camera = Camera(args)  # use 0 for web camera
+    trt_thread = TrtThread(condition, camera, args.model, args.category_num, args.letter_box, conf_th=0.3)  ## Added for async (09.14) 
+    trt_thread.start() #start the child thread
+
+    app.run(host='0.0.0.0', port=5000, debug=False, threaded=False)
+    trt_thread.stop()
